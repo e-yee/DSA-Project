8899987036dda6f66b9a583eb190d658fef0a450
phmkhoi <phanhuynhminhkhoi@gmail.com>
2024-06-27 12:39:28 +0700

    Sorting (Done), Command (In processing)

---
+++ DSA-Project/Source/DataGenerator.h
@@ -0,0 +1,12 @@
+// Hàm phát sinh mảng dữ liệu ngẫu nhiên
+void generateRandomData(int a[], int n);
+
+// Hàm phát sinh mảng dữ liệu có thứ tự tăng dần
+void generateSortedData(int a[], int n);
+
+// Hàm phát sinh mảng dữ liệu có thứ tự ngược (giảm dần)
+void generateReverseData(int a[], int n);
+
+// Hàm phát sinh mảng dữ liệu gần như có thứ tự
+void generateNearlySortedData(int a[], int n);
+void generateData(int a[], int n, int data_type);
