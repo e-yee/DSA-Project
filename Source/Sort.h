b2bfc4b19110473437e7a9da850f048380db9538
e-yee <viduydmm@gmail.com>
2024-06-28 11:57:00 +0700

    Update sorting algorithms

--- Source/Sort.h
+++ Source/Sort.h
@@ -3,9 +3,11 @@
 void selectionSort(int a[], int n, long long &cnt_cmp);
 void insertionSort(int a[], int n, long long &cnt_cmp);
 void bubbleSort(int a[], int n, long long &cnt_cmp);
+void heapSort(int a[], int n, long long &cnt_cmp);
 void mergeSort(int a[], int n, long long &cnt_cmp);
 void countingSort(int a[], int n, long long &cnt_cmp);
-
+void radixSort(int a[], int n, long long &cnt_cmp);
+void flashSort(int a[], int n, long long &cnt_cmp);
 
 
 // ----------------------- calculate running time -----------------------
@@ -13,5 +15,8 @@ void countingSort(int a[], int n, long long &cnt_cmp);
 void selectionSort(int a[], int n, double &duration);
 void insertionSort(int a[], int n, double &duration);
 void bubbleSort(int a[], int n, double &duration);
+void heapSort(int a[], int n, double &duration);
 void mergeSort(int a[], int n, double &duration);
 void countingSort(int a[], int n, double &duration);
+void radixSort(int a[], int n, double &duration);
+void flashSort(int a[], int n, double &duration);
