caae822a88b18b8a7560dc1c3cc7feb21d6f1c91
Willpact <145360715+Willpact@users.noreply.github.com>
2024-06-28 14:44:46 +0700

    Revert "Merge branch 'main' of https://github.com/e-yee/DSA-Project"
    This reverts commit c956ada090b510ded0dc012722675403992341b7, reversing
    changes made to d10c906eeed3bcc6dc3ecc9fe8840578ee0c00ef.

--- Source/Sort.h
+++ Source/Sort.h
@@ -3,11 +3,9 @@
 void selectionSort(int a[], int n, long long &cnt_cmp);
 void insertionSort(int a[], int n, long long &cnt_cmp);
 void bubbleSort(int a[], int n, long long &cnt_cmp);
-void heapSort(int a[], int n, long long &cnt_cmp);
 void mergeSort(int a[], int n, long long &cnt_cmp);
 void countingSort(int a[], int n, long long &cnt_cmp);
-void radixSort(int a[], int n, long long &cnt_cmp);
-void flashSort(int a[], int n, long long &cnt_cmp);
+
 
 
 // ----------------------- calculate running time -----------------------
@@ -15,8 +13,5 @@ void flashSort(int a[], int n, long long &cnt_cmp);
 void selectionSort(int a[], int n, double &duration);
 void insertionSort(int a[], int n, double &duration);
 void bubbleSort(int a[], int n, double &duration);
-void heapSort(int a[], int n, double &duration);
 void mergeSort(int a[], int n, double &duration);
 void countingSort(int a[], int n, double &duration);
-void radixSort(int a[], int n, double &duration);
-void flashSort(int a[], int n, double &duration);
